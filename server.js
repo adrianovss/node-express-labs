7e3ea0aec1ed434a7a3be0e75adf6f876876e509
Adriano Valerio Santos Da Silva <adriano.valerio@n0mydhwtw03tj.boticario.net>
2023-10-30 16:31:37 -0300

    feat: add port const

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+    console.log(`Server Started at ${PORT}`)
+});
